19c2c19b7f6465923b7d1266eaa858f469854114
Othmane Mansouri <othmane.mansouri@outlook.com>
2024-12-07 00:37:59 +0100

    hero section with responsive illustration

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -7,25 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
+  imagePath = 'illustration.svg';
   headline = 'Learn, Compete, and Have Fun!';
   subheadline =
     'Dive into thousands of quizzes across diverse topics, test your knowledge, and compete with friends';
   keyFeatures = [
     {
       id: 1,
-      name: 'Choose your challenge',
-      description: 'From pop culture to science, there’s a quiz for everyone',
+      name: 'Choose your challenge : From pop culture to science, there’s a quiz for everyone',
     },
     {
       id: 2,
-      name: 'Customizable gameplay',
-      description: 'Pick your category and difficulty to suit your mood',
+      name: 'Customizable gameplay : Pick your category and difficulty to suit your mood',
     },
     {
       id: 3,
-      name: null,
-      description:
-        'Prove your skills and climb the leaderboard while learning something new',
+      name: 'Prove your skills and climb the leaderboard while learning something new',
     },
   ];
   callToAction = 'Start Your First Quiz';
